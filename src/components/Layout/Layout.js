6b9b076aca8bbbbaaf297e6f6b3aa74faa3882ed
Saurabh Kumar <saurabh.cse04@gmail.com>
2020-04-27 14:21:55 +0530

    🌘

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Aux from '../../hoc/Aux'
 import classes from './Layout.css'
-
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 const Layout = (props) => (
     <Aux>
+        <Toolbar/>
         <div>
             toolbar, sidebar, backdrop
         </div>
