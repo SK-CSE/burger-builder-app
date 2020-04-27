621d860c402d61ab3c59e0b83842f91c990786c0
Saurabh Kumar <saurabh.cse04@gmail.com>
2020-04-27 14:57:39 +0530

    🌐

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,7 +5,9 @@ import NavigationItems from '../NavigationItems/NavigationItems'
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>Menu</div>
-        <Logo/>
+        <div className={classes.Logo}>
+            <Logo/>
+        </div>
         <nav>
             <NavigationItems/>
         </nav>
