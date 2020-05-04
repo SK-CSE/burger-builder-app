cf9579d8f2fcd61bb9e573480364e37a8093e478
Saurabh Kumar <saurabh.cse04@gmail.com>
2020-05-04 15:15:19 +0530

    🐷

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, {Component}from 'react'
 import Aux from '../../hoc/Aux'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
-const Layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <div>
-            toolbar, sidebar, backdrop
-        </div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-    )
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState)=> {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+    render(){
+        return (
+            <Aux>
+            <Toolbar drawerToggleClicked = {this.sideDrawerToggleHandler}/>
+            <SideDrawer 
+                open = {this.state.showSideDrawer} 
+                closed = {this.sideDrawerClosedHandler}/>
+            <div>
+                toolbar, sidebar, backdrop
+            </div>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>
+        )
+    }
+}
 
 export default Layout
---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const drawerToggle = (props) =>(
+
+    <div onClick={props.clicked}>Menu</div>
+)
+
+export default drawerToggle
